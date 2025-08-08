3c26f9b243ccfd08d682251c9e7b0a77ef19d56c
marketing-space <admin@marketingspace.in>
2025-08-08 12:56:46 +0530

    first commit

---
+++ home.php
@@ -0,0 +1,277 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Homesfy</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons 
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon"> -->
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Property Search Section ======= -->
+  <div class="click-closed"></div>
+  <!--/ Form Search Star /-->
+  <div class="box-collapse">
+    <span class="close-box-collapse right-boxed bi bi-x"></span>
+    <div class="box-collapse-wrap form">
+      <ul class="navbar-nav">
+
+        <li class="nav-item">
+          <a class="nav-link active" href="index.html">Home</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="about.html">About Us</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="property-grid.html">Careers</a>
+        </li>
+
+        <li class="nav-item">
+          <a class="nav-link " href="contact.html">Contact Us</a>
+        </li>
+      </ul>
+    </div>
+  </div><!-- End Property Search Section -->
+
+  <!-- ======= Header/Navbar ======= -->
+  <nav class="navbar navbar-default navbar-trans navbar-expand-lg fixed-top">
+    <div class="container">
+     <div class="col-sm-4">
+      <a class="navbar-brand desktop" href="index.html"><img src="./assets/img/HomesfyLogo.svg"></span></a>
+      <a class="navbar-brand mobile" href="index.html"><img src="./assets/img/Circle.svg"></span></a>
+     </div>
+
+     <div class="col-sm-6">
+      <div class="navbar-collapse" style="float: right;">
+        <div class="contact-nav">
+          <div class="desktop-nav">
+            <ul>
+              <li><a href="tel:7411782414" class="desktophref">+91 74117 82414</a> </li>
+              <li><a class="call-back" href="#contained-buttons"> <span class="MuiButton-label">Get a call back</span> <span class="MuiTouchRipple-root"></span> </a> </li>
+            </ul>
+          </div>
+
+          <div class="mobile-nav">
+            <ul>
+              <li><a href="tel:7411782414" class="mobilehref"><i class="bi bi-telephone-fill"></i></a> </li>
+              <li><a class="call-back" href="#contained-buttons"> <span class="MuiButton-label"> <i class="bi bi-mouse2-fill"></i> </span> <span class="MuiTouchRipple-root"></span> </a> </li>
+            </ul>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-2" style="text-align: right;">
+      <button type="button" class="btn btn-b-n navbar-toggle-box navbar-toggle-box-collapse" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01">
+        <i class="bi bi-list"></i></i>
+      </button>
+    </div>
+
+    </div>
+  </nav><!-- End Header/Navbar -->
+  
+  
+  
+  
+  
+ <!-- ======= thankyou Section ======= -->
+    <section class="thankyou section-t8">
+      <div class="container">
+        <div class="row">
+          
+			<h2>Thanks for submit form we will contact you shortly.</h2>
+         
+        </div>
+      </div>
+    </section><!-- End tankyou Section -->
+	
+	
+	
+  
+  
+  <!-- ======= Footer ======= -->
+  <section class="section-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-3">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h4 class="w-title-a text-brand">Overview</h4>
+            </div>
+            <div class="w-body-a">
+              <ul>
+                <li><a href="/about.html">About Us</a></li>
+                <li><a href="#">Careers</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Terms & Conditions</a></li>
+                <li><a href="#">Policy of Use</a></li>
+                <li><a href="#">About RERA</a></li>
+              </ul>
+
+            </div>
+            
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h4 class="w-title-a text-brand">Address</h4>
+            </div>
+            <div class="w-body-a">
+              <div class="w-body-a">
+                <ul class="list-unstyled">
+                  <li class="item-list-a">
+                      <h5 class="mumbai"><b>Mumbai</b></h5>
+                      <h6>1601, Dev Corpora, Cadbury Junction, Thane(W) 400601</h6>
+                  </li>
+
+                  <li class="item-list-a">
+                    <h5 class="mumbai"><b>Pune</b></h5>
+                    <h6>Icon tower, 8th floor, Baner Road, Opp KFC, near Dmart, Pune 411007</h6>
+                  </li>
+
+                  <li class="item-list-a">
+                    <h5 class="mumbai"><b>Bangalore</b></h5>
+                    <h6>24, Myworkarea, Benaka Complex, 2nd Cross, Sirur Park Road, Sheshadripuram, Bangalore 560020 </h6>
+                  </li>
+
+                  <li class="item-list-a">
+                    <h5 class="mumbai"><b>Gurugram</b></h5>
+                    <h6>One Avenue 301, 3rd floor, Greenwood plaza, sector 45, Gurgaon - 122003</h6>
+                  </li>
+
+                  <li class="item-list-a">
+                    <h5 class="mumbai"><b>Noida</b></h5>
+                    <h6>415 - Tower -1 Assotech Business Park Sector 135, Noida, Uttar Pradesh 201304</h6>
+                  </li>
+
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-3">
+          <div class="widget-a">
+            <div class="w-header-a">
+              <h4 class="w-title-a text-brand">Keep In Touch</h4>
+            </div>
+            <div class="w-body-a">
+              <ul class="social-list">
+                <li class="item-list-a">
+                  <a href="#"> <i class="bi bi-facebook"></i> </a>
+                </li>
+
+                <li class="item-list-a">
+                  <a href="#"> <i class="bi bi-linkedin"></i> </a>
+                </li>
+
+                <li class="item-list-a">
+                  <a href="#"> <i class="bi bi-instagram"></i> </a>
+                </li>
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <footer>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-4">
+            <div class="footer-bottom align-left">
+              <img src="./assets/img/Whitelogo.svg" alt="footer-bottom-section">
+            </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="footer-bottom">
+            <img src="./assets/img/BYOB_White.svg" alt="footer-bottom-section">
+          </div>
+        </div>
+
+        <div class="col-md-4">
+          <div class="footer-bottom align-right">
+            <img src="./assets/img/magnetwhiteLogo.svg" alt="footer-bottom-section">
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </footer><!-- End  Footer -->
+
+  <div id="preloader"></div>
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+</html>
+
+
+
+<?php
+$to = "info@marketingspace.co.in";
+$subject = "HTML email";
+
+$message = "
+<html>
+<head>
+<title>HTML email</title>
+</head>
+<body>
+<p>This email contains HTML Tags!</p>
+<table>
+<tr>
+<th>Firstname</th><td>".$_REQUEST['name']."</td></tr>
+<tr><th>E-mail</th><td>".$_REQUEST['email']."</td></tr>
+<tr><th>Mobile</th><td>".$_REQUEST['mobile']."</td></tr>
+<tr><th>message</th><td>".$_REQUEST['message']."</td></tr>
+
+</table>
+</body>
+</html>";
+
+
+// Always set content-type when sending HTML email
+$headers = "MIME-Version: 1.0" . "\r\n";
+$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+// More headers
+$headers .= 'From: <marketingspace.co.in>' . "\r\n";
+
+
+mail($to,$subject,$message,$headers);
+?>
